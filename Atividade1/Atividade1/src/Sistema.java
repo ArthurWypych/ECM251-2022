14f6018cc536f277988a82973dd765097ac9c9a9
ArthurWypych <arthurchuck1@gmail.com>
2022-04-28 12:21:12 -0300

    sla men

--- Atividade1/Atividade1/src/Sistema.java
+++ Atividade1/Atividade1/src/Sistema.java
@@ -2,13 +2,16 @@
 
 public class Sistema {
     public void run(){
-        Carro c = new Carro(99.99);
-        Bicicleta b = new Bicicleta(19.99);
-        Usuario u1 = new Usuario("Narutinho", c);
+        Carro carro = new Carro(99.99);
+        Bicicleta bicicleta = new Bicicleta(19.99);
+        Patinete patinete = new Patinete(18.99);
+        Moto moto = new Moto(59.99);
+        Usuario u1 = new Usuario("Narutinho", carro);
         System.out.println(u1.getNome());
-        u1.trocar(b);
+        u1.trocar(bicicleta);
         u1.setNome("Saaaaaaaasuke");
-        u1.trocar(c);
+        u1.trocar(patinete);
         System.out.println(u1.getNome());
+        u1.trocar(moto);
     }
 }
